a268a5e9e660090469cfffa225a7d95eebdda296
DaniLamb01 <daniel.lambrecht@student.reutlingen-university.de>
2022-04-08 18:59:50 +0200

    removed firebase credentials

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBDWr_geqroXLbFbHRAv1FImuh_lzUELFw",
-  authDomain: "exercise-react-native-b374e.firebaseapp.com",
-  projectId: "exercise-react-native-b374e",
-  storageBucket: "exercise-react-native-b374e.appspot.com",
-  messagingSenderId: "871377188684",
-  appId: "1:871377188684:web:bf9b603d099e271ff941e9"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
